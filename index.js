fa7d746998eb34edbb8140adcbcdaf4f37d83227
Carmen Mendez <ma.carmenmendez2404@gmail.com>
2021-06-16 18:09:59 -0500

    refactor to oop

--- index.js
+++ index.js
@@ -1,79 +1,110 @@
-let tiles= [];
-let isXTurn = true;
+class Game {
+    constructor() {
+        console.log('he creado una instancia del juego')
+        this.board = new Board(this.handleClicks.bind(this))
+    }
 
-const getTurn = () => isXTurn ? 'x' : 'o';
+    isXturn = true
+    
+    // executed by Board but "this" is bind to Game
+    handleClicks(index) {
+        if (this.board.tiles[index]) return;
+        this.board.tiles[index] = this.getTurn()
+        this.isXturn = !this.isXturn;
+        this.updateHeader(`Ahora es turno de ${this.getTurn()}`);
+        this.board.renderBoard();
+        const winner = this.board.getWinner(); // undefined, 'o', 'x'
+        const isDraw = this.board.allTilesFilled();
+        if(winner) {
+            this.updateHeader(`${winner.toUpperCase()} ha ganado el juego`);
+            this.toggleRestart(true);
+        } else if (isDraw) {
+            this.updateHeader('Ups! buen juego pero fue un empate!');
+            this.toggleRestart(true)
+        }
+    }
 
-function handleClick(index) {
-    if (tiles[index]) return;
+    toggleRestart(visibility = false) {
+        const buttonRestartElement = document.querySelector('button.restart');
+        buttonRestartElement.style.display = visibility ? 'block' : 'none';
+    }
+
+    getTurn() {
+        return this.isXturn ? 'x' : 'o';
+    }
+
+    updateHeader(text = '') {
+        const headerElement = document.querySelector('.interface h1')
+        headerElement.innerText = text;
+    }
     
-    // ternary operator
-    tiles[index] = getTurn();
-    isXTurn = !isXTurn;
-    updateHeader(`Ahora es turno de ${getTurn()}`);
-    renderBoard();
-    
-    // validar al ganador
-    const winner = getWinner();
-    const isDraw = tiles.every((tile) => tile);
-    
-    if (winner) {
-        updateHeader(`${winner.toUpperCase()} ha ganado el juego`);
-        toggleRestart(true);
-    } else if (isDraw) {
-        updateHeader('Ups! buen juego pero fue un empate!');
-        toggleRestart(true);
+    startGame() {
+        console.log('juego iniciado...')
+        this.toggleRestart(false);
+        const initMessage = `Bienvenidos a Tic Tac Toe, inicia el juego ${this.getTurn()}`
+        this.updateHeader(initMessage);
+        this.board.reset();
+        this.isXturn = true;
+        this.board.renderBoard();
     }
 }
 
-function getWinner() {
-    const solutions = [
-        [0,1,2], [3,4,5], [6,7,8], //horizontales
-        [0,3,6], [1,4,7], [2,5,8], //verticales
-        [0,4,8], [2,4,6], //diagonales
-    ];
-    let winner;
-    solutions.forEach((solution) => {
-        const [a,b,c] = solution; // destructure assignment
-        const value = tiles[a];
-        const isMatch = tiles[a] === tiles[b] && tiles[a] === tiles[c];
-        if (value && isMatch) {
-            winner = value
+class Board {
+    constructor(externalClickHandler){
+        console.log('he creado una instancia del tablero')
+        this.tiles = new Array(9).fill(null);
+        this.externalClickHandler = externalClickHandler
+    }
+
+    allTilesFilled() {
+        return this.tiles.every(tile => tile)
+    }
+
+    reset() {
+        this.tiles = new Array(9).fill(null);
+    }
+
+    handleClick(index) {
+        if(typeof this.externalClickHandler === 'function') {
+            this.externalClickHandler(index)
         }
-    })
-    return winner;
+    }
+
+    getWinner() {
+        const solutions = [
+            [0,1,2], [3,4,5], [6,7,8], //horizontales
+            [0,3,6], [1,4,7], [2,5,8], //verticales
+            [0,4,8], [2,4,6], //diagonales
+        ];
+        let winner;
+        solutions.forEach((solution) => {
+            const [a,b,c] = solution;
+            const value = this.tiles[a];
+            const isMatch = this.tiles[a] === this.tiles[b] && this.tiles[a] === this.tiles[c];
+            if (value && isMatch) {
+                winner = value
+            }
+        })
+        return winner;
+    }
+   
+    renderBoard() {
+        const boardElement = document.querySelector('.board');
+        const buttons = this.tiles.map((tile, index) => {
+            const button = document.createElement('button');
+            const markClass = tile === 'x' ? 'mark-x' : 'mark-o';
+            button.classList = `tile ${tile ? markClass : ''}`;
+            button.innerText = tile;
+            button.onclick = () => this.handleClick(index);
+            return button;
+        });
+        boardElement.replaceChildren(...buttons);
+    };
+
+
 }
 
-function renderBoard() {
-    const boardElement = document.querySelector('.board');
-    const buttons = tiles.map((tile, index) => {
-        const button = document.createElement('button');
-        const markClass = tile === 'x' ? 'mark-x' : 'mark-o';
-        button.classList = `tile ${tile ? markClass : ''}` ;
-        button.innerText = tile;
-        button.onclick = () => handleClick(index);
-        return button;
-    });
-    boardElement.replaceChildren(...buttons);
-};
+const game = new Game()
+game.startGame()
+console.log(game)
 
-function updateHeader(text) {
-    const headerElement = document.querySelector('.interface h1');
-    headerElement.innerText = text || `Bienvenidos a Tic Tac Toe, inicia el juego ${getTurn()}`;
-}
-
-function toggleRestart(visibility = false) {
-    const buttonRestartElement = document.querySelector('button.restart');
-    buttonRestartElement.style.display = visibility ? 'block' : 'none';
-}
-
-function startGame() {
-    //inicializar valores
-    tiles= new Array(9).fill(null);
-    isXTurn = true;
-    //update UI
-    toggleRestart(false);
-    updateHeader();
-    renderBoard();
-}
-
-startGame();
